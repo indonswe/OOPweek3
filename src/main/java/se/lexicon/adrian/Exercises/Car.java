c0bbc943ac64812126f7025272675975c5afbd7d
indonswe <afc2002afc@hotmail.com>
2020-11-09 15:04:02 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/adrian/Exercises/Car.java
@@ -0,0 +1,11 @@
+package se.lexicon.adrian.Exercises;
+
+public class Car {
+    private int id;
+    private int year;
+    private String model;
+    private String colour;
+    private int motorSize;
+
+
+}
